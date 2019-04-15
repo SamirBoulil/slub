0ac4f2d07c0ef4366af3752be85bbc7e3c6ea2cd
Samir Boulil <samir.boulil@akeneo.com>
2019-04-16 01:03:49 +0200

    Add new event PRPutToReview on PR creation

---
+++ src/Slub/Domain/Event/PRPutToReview.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Slub\Domain\Event;
+
+use Slub\Domain\Entity\PR\MessageIdentifier;
+use Slub\Domain\Entity\PR\PRIdentifier;
+use Symfony\Component\EventDispatcher\Event;
+use Webmozart\Assert\Assert;
+
+/**
+ * @author    Samir Boulil <samir.boulil@akeneo.com>
+ */
+class PRPutToReview extends Event
+{
+    /** @var PRIdentifier */
+    private $PRIdentifier;
+
+    /** @var MessageIdentifier */
+    private $messageIdentifier;
+
+    private function __construct(PRIdentifier $PRIdentifier, MessageIdentifier $messageIdentifier)
+    {
+        $this->PRIdentifier = $PRIdentifier;
+        $this->messageIdentifier = $messageIdentifier;
+    }
+
+    public static function forPR(PRIdentifier $PRIdentifier, MessageIdentifier $messageIdentifier): self
+    {
+        return new self($PRIdentifier, $messageIdentifier);
+    }
+
+    public function PRIdentifier(): PRIdentifier
+    {
+        return $this->PRIdentifier;
+    }
+
+    public function messageIdentifier(): MessageIdentifier
+    {
+        return $this->messageIdentifier;
+    }
+}

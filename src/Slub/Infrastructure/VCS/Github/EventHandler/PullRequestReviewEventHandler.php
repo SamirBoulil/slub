d0b15c09d8988510a955de040b13ba115ffe9c8b
Samir Boulil <samir.boulil@akeneo.com>
2020-03-07 11:36:18 +0100

    Improve Infra layer to retrieve reviewername from reviews

--- src/Slub/Infrastructure/VCS/Github/EventHandler/PullRequestReviewEventHandler.php
+++ src/Slub/Infrastructure/VCS/Github/EventHandler/PullRequestReviewEventHandler.php
@@ -44,6 +44,7 @@ class PullRequestReviewEventHandler implements EventHandlerInterface
         $newPRReview->PRIdentifier = $this->getPRIdentifier($PRStatusUpdate);
         $newPRReview->repositoryIdentifier = $this->getRepositoryIdentifier($PRStatusUpdate);
         $newPRReview->reviewStatus = $this->reviewStatus($PRStatusUpdate);
+        $newPRReview->reviewerName = $this->reviewerName($PRStatusUpdate);
 
         return $newPRReview;
     }
@@ -82,5 +83,10 @@ class PullRequestReviewEventHandler implements EventHandlerInterface
     private function authorReviewedHisOwnPR($PRStatusUpdate): bool
     {
         return $PRStatusUpdate['review']['user']['id'] === $PRStatusUpdate['pull_request']['user']['id'];
+    }
+
+    private function reviewerName(array $PRStatusUpdate): string
+    {
+        return $PRStatusUpdate['review']['user']['login'];
     }
 }

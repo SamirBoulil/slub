bbd1564c221a0179d6b2705885eb022ab6521d21
Pierrick Martos <pierrick@martos.me>
2021-10-13 11:01:56 +0200

    refactor PR too large logic

--- src/Slub/Application/WarnLargePR/WarnLargePRHandler.php
+++ src/Slub/Application/WarnLargePR/WarnLargePRHandler.php
@@ -36,39 +36,50 @@ class WarnLargePRHandler
         $this->prSizeLimit = $prSizeLimit;
     }
 
-    public function handle(WarnLargePR $WarnLargePR): void
+    public function handle(WarnLargePR $warnLargePR): void
     {
-        if (!$this->isSupported($WarnLargePR)) {
+        if (!$this->isSupported($warnLargePR)) {
             return;
         }
-        $this->warnLargePR($WarnLargePR);
-        $this->logIt($WarnLargePR);
+        $this->warnLargePR($warnLargePR);
+        $this->logIt($warnLargePR);
     }
 
-    private function isSupported(WarnLargePR $WarnLargePR): bool
+    private function isSupported(WarnLargePR $warnLargePR): bool
     {
-        $repositoryIdentifier = RepositoryIdentifier::fromString($WarnLargePR->repositoryIdentifier);
+        $repositoryIdentifier = RepositoryIdentifier::fromString($warnLargePR->repositoryIdentifier);
 
         return $this->isSupported->repository($repositoryIdentifier);
     }
 
-    private function warnLargePR(WarnLargePR $WarnLargePR): void
+    private function warnLargePR(WarnLargePR $warnLargePR): void
     {
-        $PR = $this->PRRepository->getBy(PRIdentifier::fromString($WarnLargePR->PRIdentifier));
-        if ($WarnLargePR->additions > $this->prSizeLimit || $WarnLargePR->deletions > $this->prSizeLimit) {
+        $PR = $this->PRRepository->getBy(PRIdentifier::fromString($warnLargePR->PRIdentifier));
+        if ($this->isPRTooLarge($warnLargePR)) {
             $PR->large();
-        } else if ($WarnLargePR->additions <= $this->prSizeLimit && $WarnLargePR <= $this->prSizeLimit) {
+        } else {
             $PR->small();
         }
 
         $this->PRRepository->save($PR);
     }
 
-    private function logIt(WarnLargePR $WarnLargePR): void
+    private function logIt(WarnLargePR $warnLargePR): void
     {
-        if ($WarnLargePR->additions > $this->prSizeLimit || $WarnLargePR->deletions > $this->prSizeLimit) {
-            $logMessage = sprintf('Author has been notified PR "%s" is too large', $WarnLargePR->PRIdentifier);
+        if ($this->isPRTooLarge($warnLargePR)) {
+            $logMessage = sprintf('Author has been notified PR "%s" is too large', $warnLargePR->PRIdentifier);
             $this->logger->info($logMessage);
         }
+    }
+
+    private function isPRTooLarge(WarnLargePR $warnLargePR)
+    {
+        if ($warnLargePR->additions > $this->prSizeLimit || $warnLargePR->deletions > $this->prSizeLimit) {
+            return true;
+        } else if ($warnLargePR->additions <= $this->prSizeLimit && $warnLargePR <= $this->prSizeLimit) {
+            return false;
+        }
+
+        return false;
     }
 }
